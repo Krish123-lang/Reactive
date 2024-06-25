3c8de94b0fdc8764f6e4f28eff251e26f762aa48
Krishna Kumar Mandal <mandalkrishna739@gmail.com>
2024-06-25 17:27:47 +0545

    Destructuring

--- src/components/PropsPractice.jsx
+++ src/components/PropsPractice.jsx
@@ -1,16 +1,17 @@
-export const PropsPractice = (props) => {
+export const PropsPractice = ({ curElem }) => {
+
+    const { id, name, img_url, rating, description, cast, genre, watch_url } = curElem;
+
     return (
         <>
             <li>
-                <img src={props.curElem.img_url} alt={props.curElem.name} width={150} height={100} />
-                <h1>{props.curElem.name}</h1>
-                <p>{props.curElem.description}</p>
-                <b>{props.curElem.rating}</b>
-                <p>{props.curElem.cast}</p>
-                <p>{props.curElem.genre}</p>
-                <a href={props.curElem.watch_url} target="_blank">
-                    <button>Watch now</button>
-                </a>
+                <img src={img_url} alt={name} width={150} height={100} />
+                <h1>{name}</h1>
+                <p>{description}</p>
+                <b>{rating}</b>
+                <p>{cast}</p>
+                <p>{genre}</p>
+                <a href={watch_url} target="_blank"><button>Watch now</button></a>
             </li>
         </>
     )
